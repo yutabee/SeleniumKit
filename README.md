d94974d2e3a626e5036b3872dad3baadf4d7192b
yutabee <bsk87m@gmail.com>
2024-04-18 17:29:01 +0900

    Update README.md and requirements.txt, and add initialize_driver function to seleniumkit/core.py

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SeleniumKit
+
+## ChromeDriverのインストール:
+- ChromeDriverは [ChromeDriverのダウンロードページ](https://sites.google.com/a/chromium.org/chromedriver/) からダウンロードしてください。
+- ダウンロードしたファイルを解凍し、実行可能ファイルをPATHが通っているディレクトリに配置してください。
+
+## 
--- seleniumkit/core.py
+++ seleniumkit/core.py
@@ -5,21 +5,32 @@ from selenium.webdriver.chrome.options import Options
 
 def initialize_driver(headless: bool = True) -> webdriver.Chrome:
     """
-    WebDriverを初期化する関数
+    Selenium WebDriverを初期化する関数
+
+    Args:
+        headless (bool): ブラウザをヘッドレスモードで起動するかどうか。
+    
+    Returns:
+        webdriver.Chrome: 初期化されたWebDriverインスタンス
+        
+    Raises:
+        WebDriverException: WebDriverの初期化またはURLのオープン中にエラーが発生した場合
+        
+    使用例:
+        >>> driver = initialize_driver(headless=True)
+        >>> driver.get('https://www.google.com')
+        >>> driver.quit()
     """
     try:
         chrome_options = Options()
         if headless:
             chrome_options.add_argument("--headless")
-        # ログの出力レベルを設定
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        # メモリの使用量を抑えるために必要なオプション
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--disable-software-rasterizer")
-        # ユーザーエージェントを指定
         chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36")
-        # ドライバーの初期化
+
         driver = webdriver.Chrome(options=chrome_options)
 
         print('initialize driver success')
@@ -28,3 +39,8 @@ def initialize_driver(headless: bool = True) -> webdriver.Chrome:
     except WebDriverException as e:
         print(f"An error occurred while initializing the WebDriver or opening the URL: {e}")
         raise
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        raise
+
+  
