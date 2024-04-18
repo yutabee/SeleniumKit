3c94308b169af2246498fa1219544cb8da4ebdce
yutabee <bsk87m@gmail.com>
2024-04-18 18:14:56 +0900

    Refactor seleniumkit/core.py: Remove unused import statement

--- seleniumkit/core.py
+++ seleniumkit/core.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
